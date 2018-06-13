e4c661e0c31c058255b29b3d6cb9dce1c7127eb5
Nate Miller <natekmiller96@gmail.com>
2018-06-13 22:18:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,12 +10,9 @@ end
 
 def current_player(board)
   filled = turn_count(board)
-  if filled % 2 = 0
+  if filled % 2 == 0
     return "X"
   else
     return "O"
   end
 end
-
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-current_player(board)
